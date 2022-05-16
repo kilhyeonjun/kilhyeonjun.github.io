e49de769b5205c699b2bdade6b991a075a0d62ce
kilhyeonjun <kboxstar@gmail.com>
2022-05-16 11:28:14 +0900

    modify config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,22 @@
 module.exports = {
   title: `Bee starter`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `kilhyeonjun`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://kilhyeonjun.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `kilhyeonjun`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `현준-길-a808aa21a/`, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`kilhyeonjun`, `blog`, `developer`, `development`, `fullstack`, `backend`, `express`, `spring`, `java`, `golang`, `go`, `frontend`, `react`, `web`, `javascript`, `typescript`,],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'kilhyeonjun/kilhyeonjun.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
